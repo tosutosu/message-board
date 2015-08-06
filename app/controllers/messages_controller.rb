c2e8a031fd0491f233a91f4f1b696f78c575d117
chidatoshihiro <toshi.c.1986@gmail.com>
2015-08-06 21:53:31 +0000

    8.6 end. I finished edit.html

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,12 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update]
+  
   def index
     # Messageを全て取得する
     @messages = Message.all
     @message = Message.new
   end
     
-  ## ここから追記
   def create
     @message = Message.new(message_params)
     if @message.save
@@ -17,10 +18,27 @@ class MessagesController < ApplicationController
       render 'index'
     end
   end
-    
-  private
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページヘリダイレクト
+      redirect_to root_path, notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は画面編集へ戻す
+      render 'edit'
+    end
+  end
+  
+private
+
   def message_params
     params.require(:message).permit(:name, :body)
   end
-  ## ここまで
+  
+  def set_message
+    @message = Message.find(params[:id])
+  end
 end
