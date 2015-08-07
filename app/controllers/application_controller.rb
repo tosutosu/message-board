423f0578ca41952ba6e0322e5f9b41ea2750aff8
chidatoshihiro <toshi.c.1986@gmail.com>
2015-08-07 17:46:19 +0000

    pre marge2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,71 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-end
+ *= require_tree .
+ *= require_self
+ */
+
+body.msg-body {
+        background-color: #dcdcdc;
+        padding: 0px;
+}
+div {
+    margin: 0px;
+    padding: 0px;
+}
+div.msg-flame {
+    display: block;
+    background-color: #ffffff;
+    margin: 7px;
+    padding-left: 10px;
+    padding-right: 5px;
+    box-shadow: 3px 3px 6px #000000;
+}
+
+h1, h2 {
+    margin: 0px;
+}
+
+textarea.msg-form-textarea {
+    border:0;
+    padding:10px;
+    font-family: 'ヒラギノ角ゴ Pro W3', 'Hiragino Kaku Gothic Pro', 'Hiragino Kaku Gothic ProN', 'メイリオ', Meiryo;
+    color:#aaa;
+    border:solid 1px #ccc;
+    margin:0px;
+    width: 100%;
+    resize: none;
+}
+input.msg-form-text {
+    font-family: 'ヒラギノ角ゴ Pro W3', 'Hiragino Kaku Gothic Pro', 'Hiragino Kaku Gothic ProN', 'メイリオ', Meiryo;
+}
+button.msg-form-btn {
+    width: 100px;
+}
+div.msg-separator {
+    padding: 10px 10px 0px;
+}
+
+div.msg-panel {
+    border-radius: 0px;
+}
+div.msg-panel-heading {
+    margin: 0px;
+    padding: 0px;
+}
+div.msg-list-group-item {
+    padding: 0px 0px 0px 5px;
+    border: none;
+}
+div.msg-list-group-item-header {
+    background-color: #f0f8ff;
+    border-bottom: solid 1px #87cefa;
+}
+div.msg-list-group-item-textarea {
+    padding-left: 10px;
+}
+span.msg-list-group-item-text {
+    font-style: italic;
+}
+hr.msg-separator {
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+ No newline at end of file
