6382dc7a81fcad0a4232eaeb4ff3d938ca4113a6
chidatoshihiro <toshi.c.1986@gmail.com>
2015-08-07 21:17:11 +0000

    format change.

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,9 +3,7 @@ class Message < ActiveRecord::Base
     validates :name, length: {maximum: 20}, presence: true
     # 内容は必須入力かつ2文字以上30文字以下
     validates :body, length: {minimum: 2, maximum: 30}, presence: true
-    #validates :age, presence: true
+    # 年齢は必須入力かつ0歳以上~150以下の数値
     validates_numericality_of :age, only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 150
 
-    #validates_numericality_of :age, only_integer: ture, greater_than_or_equal_to: 0, less_than_or_equal_to:150
-    #validates_numericality_of :age, only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to:150
 end
